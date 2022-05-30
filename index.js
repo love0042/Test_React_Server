5e5288fbdbd6d1ecca4ab4ef2ec1f66d96f0084c
love0042 <love0042@gmail.com>
2022-05-30 09:46:54 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://love0042:love0042@zentotest.kgxsrqf.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
