8c3b733739da437e5005cedfe2fb8cf08f9bbbaa
love0042 <love0042@gmail.com>
2022-05-30 11:31:36 +0900

    설정 추가

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const bodyParser = require("body-parser");
+const { User } = require("./src/models/User");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://love0042:love0042@zentotest.kgxsrqf.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!! Backend");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
